9c29f673c2a55929e41c48dd3e7ab3d39bb85603
dotn8 <dotn8@users.noreply.github.com>
2018-08-17 04:57:27 -0500

    Add a Scrip GraphQL server

---
+++ Scrip.Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using System;
+
+namespace Scrip.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var schema = Schema.For(@"
+  type Query {
+    hello: String
+  }
+");
+
+            var root = new { Hello = "Hello World!" };
+            var json = schema.Execute(_ =>
+            {
+                _.Query = "{ hello }";
+                _.Root = root;
+            });
+
+            Console.WriteLine(json);
+        }
+    }
+}
